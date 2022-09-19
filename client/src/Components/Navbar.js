968991e9623a74e38b6b47f01fb9339a243f7be6
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-09-19 17:15:24 +0100

    Added Responsiveness To Navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../App.css"
+import ReorderIcon from "@material-ui/icons/Reorder"
 
 function Navbar() {
+
+  const [showLinks, setShowLinks] = useState(false);
   return (
     <div className='Navbar'>
         <div className='leftSide'>
-            <div className='links'>    
+            <div className='links' id={showLinks ? "hidden" : ""}>    
                 <a href='/home'>Home</a>
-                <a href='/about'>What is GE</a>
+                <a href='/about'>About</a>
                 <a href='/tutorial'>Tutorial</a>
                 <a href='/documentation'>Documentation</a>
                 <a href='/community'>Community</a>
                 <a href='/download'>Download</a>
             </div>
+            <button onClick={() => setShowLinks(!showLinks)}><ReorderIcon /></button>
         </div>
         <div className='rightSide'>
             <button>Download</button>
