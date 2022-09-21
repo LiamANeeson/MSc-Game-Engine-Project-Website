5a106bcf08eb214deef33cbc3267014ce8fe91ef
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-09-21 19:47:59 +0100

    Added Skeleton For Content on Homepage

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -4,32 +4,27 @@ import './Home.css'
 function Home() {
   return (
     <div className='container'>
-      {/* <div className='headline-content'>Headline</div> */}
-      <div className='card'>
-        <div className='card-image'></div>
-          <h2>Title</h2>
-          <p>Text</p>
-          <button>Click</button>
+      <div className='headline-content'>
+        <h2 className='headline-text'>Learn Game Development</h2>
+        <div className='head-image'></div>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis dolor rutrum, facilisis lacus porttitor, maximus risus. Vivamus aliquam lorem in leo sodales dictum. In consequat elit at lobortis ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse nec suscipit justo, a pretium quam.
+        </p>
+        <button className = 'head-btn'>Click</button>
+        <button className = 'head-btn'>Click</button>
       </div>
-      <div className='card'>
-      <div className='card-image'></div>
-          <h2>Title</h2>
-          <p>Text</p>
-          <button>Click</button>
+      <div className='box-1'>
+        <h2 className='box-head'>Box 1</h2>
       </div>
-      <div className='card'>
-        <div className='card-image'></div>
-          <h2>Title</h2>
-          <p>Text</p>
-          <button>Click</button>
+      <div className='box-2'>
+      <h2 className='box-head'>Box 2</h2>
       </div>
-      <div className='card'>
-        <div className='card-image'></div>
-            <h2>Title</h2>
-            <p>Text</p>
-            <button>Click</button>
+      <div className='box-3'>
+      <h2 className='box-head'>Box 3</h2>
       </div>
-      {/* <div className='footer'>footer</div> */}
+      <div className='box-4'>
+      <h2 className='box-head'>Box 4</h2>
+      </div>
     </div>
   )
 }
