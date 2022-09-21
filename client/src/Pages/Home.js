48617fec6f655872bf3d8ec4651fa8991ccb3882
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-09-21 14:52:25 +0100

    Created Cards for Content on Homepage

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>
-
+    <div className='container'>
+      {/* <div className='headline-content'>Headline</div> */}
+      <div className='card'>
+        <div className='card-image'></div>
+          <h2>Title</h2>
+          <p>Text</p>
+          <button>Click</button>
+      </div>
+      <div className='card'>
+      <div className='card-image'></div>
+          <h2>Title</h2>
+          <p>Text</p>
+          <button>Click</button>
+      </div>
+      <div className='card'>
+        <div className='card-image'></div>
+          <h2>Title</h2>
+          <p>Text</p>
+          <button>Click</button>
+      </div>
+      <div className='card'>
+        <div className='card-image'></div>
+            <h2>Title</h2>
+            <p>Text</p>
+            <button>Click</button>
+      </div>
+      {/* <div className='footer'>footer</div> */}
     </div>
   )
 }
