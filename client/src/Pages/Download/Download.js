ffc48b0a41e5e50a5bc7dcb2ebb6b63b335ca736
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-09-25 19:37:41 +0100

    Added Basic UI to Download Page

--- client/src/Pages/Download/Download.js
+++ client/src/Pages/Download/Download.js
@@ -1,17 +1,20 @@
  import React from 'react'
+ import './Download.css'
  
  function Download() {
    return (
-     <div className='container'>
+     <div className='download-container'>
       <h2 className='title-headear'>Download Game Engine </h2>
-      <div className='download-box'>Download Box</div>
-      <div className='download-image'></div>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis dolor rutrum, facilisis lacus porttitor, maximus risus. 
-        Vivamus aliquam lorem in leo sodales dictum. In consequat elit at lobortis ultrices. 
-        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse nec suscipit justo, a pretium quam.
-      </p>
-      <button>Download Now</button>
+        <div className='download-box'>
+          <h2 className='download-head'>Download Box</h2>
+          <div className='download-image'></div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis dolor rutrum, facilisis lacus porttitor, maximus risus. 
+            Vivamus aliquam lorem in leo sodales dictum. In consequat elit at lobortis ultrices. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse nec suscipit justo, a pretium quam.
+          </p>
+          <button className='download-btn'>Download Now</button>
+        </div>
      </div>
    )
  }
