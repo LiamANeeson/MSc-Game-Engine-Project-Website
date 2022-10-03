b72fb4a4814a199382019b89e91d1ecea12bc414
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-10-03 23:13:04 +0100

    Added dependencies and folder structure

---
+++ backend/controllers/userController.js
@@ -0,0 +1,4 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv').config
-const port = 5000
+const port = process.env.PORT || 5000
 
 const app = express()
+
+app.use('/api/login', require('./routes/userRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
