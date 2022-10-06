e7545d85719967260bd7d3ea9aa3257fecb1e87c
prajaktarocks <kulkarni.prajakta2612@gmail.com>
2022-10-06 18:05:52 +0100

    Added bootstrap and UI design

---
+++ client/src/Pages/Community/QuestionDetail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Row, Col, Container, Image } from 'react-bootstrap';
+import './Community.css'
+
+const QuestionDetail = (props) => {
+
+    return (
+        <Row>
+            <Col md={12}>
+                Question detail
+            </Col>
+        </Row>
+    );
+}
+
+export default QuestionDetail;
