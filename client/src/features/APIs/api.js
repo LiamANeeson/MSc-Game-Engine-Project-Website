fb2991a08e2d905fdd78012ea01381972610fd2f
prajakta kulkarni <kulkarni.prajakta2612@gmail.com>
2022-10-27 15:53:02 +0100

    Error fixing

--- client/src/features/APIs/api.js
+++ client/src/features/APIs/api.js
@@ -162,3 +162,73 @@ export async function getAnswer(answerID) {
     return [errorObject, null];
   }
 }
+
+//delete question
+export async function deleteQuestion(questionID) {
+  try {
+    let token = localStorage.getItem("AuthToken");
+    const axiosConfig = {
+      method: "delete",
+      url: `${apiURL}/question/${questionID}`,
+      headers: { Authorization: "Bearer " + token },
+    };
+    const response = await axios.default.request(axiosConfig);
+    const normalizedResponse = normalizeServerResponse(response);
+    return [null, normalizedResponse];
+  } catch (error) {
+    const errorObject = normalizeServerError(error);
+    return [errorObject, null];
+  }
+}
+//vote question
+export async function voteQuestion(questionID) {
+  try {
+    let token = localStorage.getItem("AuthToken");
+    const axiosConfig = {
+      method: "patch",
+      url: `${apiURL}/question/${questionID}/vote`,
+      headers: { Authorization: "Bearer " + token },
+    };
+    const response = await axios.default.request(axiosConfig);
+    const normalizedResponse = normalizeServerResponse(response);
+    return [null, normalizedResponse];
+  } catch (error) {
+    const errorObject = normalizeServerError(error);
+    return [errorObject, null];
+  }
+}
+
+//down vote question
+export async function downVoteQuestion(questionID) {
+  try {
+    let token = localStorage.getItem("AuthToken");
+    const axiosConfig = {
+      method: "patch",
+      url: `${apiURL}/question/${questionID}/down_vote`,
+      headers: { Authorization: "Bearer " + token },
+    };
+    const response = await axios.default.request(axiosConfig);
+    const normalizedResponse = normalizeServerResponse(response);
+    return [null, normalizedResponse];
+  } catch (error) {
+    const errorObject = normalizeServerError(error);
+    return [errorObject, null];
+  }
+}
+//follow question
+export async function followQuestion(questionID) {
+  try {
+    let token = localStorage.getItem("AuthToken");
+    const axiosConfig = {
+      method: "patch",
+      url: `${apiURL}/question/${questionID}/follow`,
+      headers: { Authorization: "Bearer " + token },
+    };
+    const response = await axios.default.request(axiosConfig);
+    const normalizedResponse = normalizeServerResponse(response);
+    return [null, normalizedResponse];
+  } catch (error) {
+    const errorObject = normalizeServerError(error);
+    return [errorObject, null];
+  }
+}
