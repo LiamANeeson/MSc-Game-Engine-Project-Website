2db0c1da60d972521fcb59cef0dc05d7bd93460e
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-10-28 01:24:25 +0100

    Fix Routing attempt

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 
