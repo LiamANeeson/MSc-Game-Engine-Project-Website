63615ce9d1ae8efe3a5d55632a5acb221f025ff7
prajakta kulkarni <kulkarni.prajakta2612@gmail.com>
2022-10-29 14:58:49 +0100

    deployment changes

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const logger = require("morgan");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors")
 const port = process.env.PORT || 5000;
-
 connectDB()
 
 const app = express();
@@ -13,11 +12,23 @@ app.use(logger("dev"));
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
-    res.send("APIs Up && running");
+const path = require('path');
+__dirname=path.resolve();
+
+if(process.env.NODE_ENV=="production")
+{
+  app.use(express.static(path.join(__dirname,'client/build')));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+
+}
+else
+{app.get("/", (req, res) => {
+  res.send("APIs Up && running");
 });
+}
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/question",require("./routes/questionRoutes"))
 app.use("/api/answer",require("./routes/answerRoute"))
@@ -25,3 +36,5 @@ app.use("/api/answer",require("./routes/answerRoute"))
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 app.use(errorHandler);
+
+module.exports = app;
