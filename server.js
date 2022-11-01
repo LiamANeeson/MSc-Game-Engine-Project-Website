09374a8c2f1d45c6c8dd6c580803cca94704085a
prajakta kulkarni <kulkarni.prajakta2612@gmail.com>
2022-11-01 22:42:38 +0000

    fixing api routes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
+  app.get("(?!\/api)", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
