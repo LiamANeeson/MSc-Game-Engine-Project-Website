d3d987cd10c800686037644289d5ce1c3aff5484
prajakta kulkarni <kulkarni.prajakta2612@gmail.com>
2022-11-01 22:45:34 +0000

    fixing api routes after hosting

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const logger = require("morgan");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors")
 const port = process.env.PORT || 5000;
-
 connectDB()
 
 const app = express();
@@ -13,15 +12,31 @@ app.use(logger("dev"));
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
+const path = require('path');
+__dirname = path.resolve();
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("(?!\/api)", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+else {
+  app.get("/", (req, res) => {
+    console.log("This is development mode")
     res.send("APIs Up && running");
-});
+  });
+}
+
 app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/question",require("./routes/questionRoutes"))
-app.use("/api/answer",require("./routes/answerRoute"))
+app.use("/api/question", require("./routes/questionRoutes"))
+app.use("/api/answer", require("./routes/answerRoute"))
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 app.use(errorHandler);
+
+module.exports = app;
