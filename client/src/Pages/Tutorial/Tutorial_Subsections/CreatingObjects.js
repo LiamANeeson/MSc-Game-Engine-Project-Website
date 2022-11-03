9895143c24a5c8074b16dffda84c9896daffb04b
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-11-03 15:16:46 +0000

    Added Links and Pages for Tutorials and Documentation

---
+++ client/src/Pages/Tutorial/Tutorial_Subsections/CreatingObjects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../../../Components/Sidebar/Sidebar'
+
+function CreatingObjects() {
+  return (
+    <div>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default CreatingObjects
