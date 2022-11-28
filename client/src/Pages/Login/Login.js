7d33dc150dba0f85c96703ec900b06b445d2c498
Ming <gaominghui464@outlook.com>
2022-11-28 16:06:15 +0000

    log in and log out toggle button

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -9,105 +9,106 @@ import { login, reset } from "../../features/auth/authSlice";
 import "./Login.css";
 
 function Login() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
 
-  const { email, password } = formData;
+    const { email, password } = formData;
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const { user, isError, isSuccess, message } = useSelector(
-    (state) => state.auth
-  );
+    const { user, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
 
-  useEffect(() => {
-    if (isError) {
-      toast.error(message);
-    }
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
 
-    if (isSuccess || user) {
-      console.log(user,"user______________profile");
-      localStorage.setItem("userId",user._id)
-      navigate("/profile");
-    }
+        if (isSuccess || user) {
+            console.log(user, "user______________profile");
+            localStorage.setItem("userId", user._id)
+            navigate("/profile");
+            window.location.reload(false);
+        }
 
-    dispatch(reset());
-  }, [user, isError, isSuccess, message, navigate, dispatch]);
+        dispatch(reset());
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
 
-  const onChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    const userData = {
-      email,
-      password,
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
     };
-    dispatch(login(userData));
-  };
-  return (
-    <>
-      <container className="login-container">
-        <section className="head">
-          <h1>
-            Login <FaSignInAlt />
-          </h1>
-          <p>Log into your Horizon Game Engine account!</p>
-        </section>
-        <form onSubmit={onSubmit} className="submission-form">
-          <label for="email">Email</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={email}
-            placeholder="Please Enter your email"
-            onChange={onChange}
-          />
-          <label for="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            value={password}
-            placeholder="Please enter password"
-            onChange={onChange}
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              marginTop: "5px",
-            }}
-          >
-            <div>
-              <p style={{ color: "blueviolet", cursor: "pointer" }}>
-                Forget password?
-              </p>
-            </div>
-            {/* <div>
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const userData = {
+            email,
+            password,
+        };
+        dispatch(login(userData));
+    };
+    return (
+        <>
+            <container className="login-container">
+                <section className="head">
+                    <h1>
+                        Login <FaSignInAlt />
+                    </h1>
+                    <p>Log into your Horizon Game Engine account!</p>
+                </section>
+                <form onSubmit={onSubmit} className="submission-form">
+                    <label for="email">Email</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        value={email}
+                        placeholder="Please Enter your email"
+                        onChange={onChange}
+                    />
+                    <label for="password">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        value={password}
+                        placeholder="Please enter password"
+                        onChange={onChange}
+                    />
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            marginTop: "5px",
+                        }}
+                    >
+                        <div>
+                            <p style={{ color: "blueviolet", cursor: "pointer" }}>
+                                Forget password?
+                            </p>
+                        </div>
+                        {/* <div>
               <p style={{ color: "blueviolet", cursor: "pointer" }} onClick={resetPassword} >
                 Reset password
               </p>
             </div> */}
-          </div>
-          <button type="submit" className="submit-btn">
-            Submit
-          </button>
-        </form>
-      </container>
-    </>
-  );
+                    </div>
+                    <button type="submit" className="submit-btn">
+                        Submit
+                    </button>
+                </form>
+            </container>
+        </>
+    );
 }
 
 export default Login;
