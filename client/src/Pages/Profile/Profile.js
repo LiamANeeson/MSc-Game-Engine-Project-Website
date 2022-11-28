3d17cb99c4e0cf63b2dd72b76171eab4340fa96b
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-11-28 19:27:12 +0000

    Fixed Errors on Profile Front end

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -1,23 +1,19 @@
-import React, { useState } from "react";
+import React, {useState} from 'react'
 import "./Profile.css"
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { logout, reset } from '../../features/auth/authSlice'
 import {
-    Container,
-    Row,
-    Col,
-    Card,
-    Button,
-    ListGroup,
-    ListGroupItem
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Modal
 } from 'react-bootstrap'
-import Button from "react-bootstrap/Button";
-import "./Profile.css";
-import { useSelector, useDispatch } from "react-redux";
-import bootstrap from "bootstrap";
 import * as Api from "../../features/APIs/api";
-import Modal from "react-bootstrap/Modal";
 import { toast } from "react-toastify";
 
 
@@ -25,10 +21,9 @@ function Profile() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-
-    const currentProfile = JSON.parse(localStorage.getItem('profile'))
-    const userName = JSON.parse(localStorage.getItem('userName'))
-    const email = JSON.parse(localStorage.getItem('email'))
+  const currentProfile = JSON.parse(localStorage.getItem('profile'))
+  const userName = JSON.parse(localStorage.getItem('userName'))
+  const email = JSON.parse(localStorage.getItem('email'))
 
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
@@ -36,6 +31,7 @@ function Profile() {
   const onChangeOldPassword = (e) => {
     setOldPassword(e.target.value);
   };
+
   const onChangeNewPassword = (e) => {
     setNewPassword(e.target.value);
   };
@@ -43,10 +39,7 @@ function Profile() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const currentProfile = JSON.parse(localStorage.getItem("profile"));
-  const userName = JSON.parse(localStorage.getItem("userName"));
-
+  
   const toUpdateProfile = () => {
     navigate("/updateProfile");
   };
@@ -62,28 +55,30 @@ function Profile() {
   };
 
   const resetPassword = async () => {
-    const USerId = localStorage.getItem("userId");
-    if (oldPassword && newPassword) {
+    const UserId = localStorage.getItem("userId");
+    if (oldPassword && newPassword){
       const [err, res] = await Api.resetPass(
         oldPassword,
         newPassword,
-        // "637763d57d0a8e868cff774a"
-        USerId
-      );
+        UserId
+      )
       if (err) {
         console.log(err);
         if (err?.data === "Request failed with status code 400") {
-          toast.error("Old password does not match");
-        }else{
-            toast.error("Something went wrong");
+          toast.error("Old Passwrod does not match");
+        }
+        else
+        {
+          toast.error("Something went wrong ")
         }
       }
       handleClose();
-      toast.info(res?.data?.message);
+      toast.info(res?.data?.message)
     }
+  }
+
   return (
     <>
-            <section>
             <Container className='py-5'>
                 <Row>
                     <Col lg ="4">
@@ -93,7 +88,48 @@ function Profile() {
                                 <div className = 'd-flex justify-content-center mb-2'>
                                     <Button outline className="ms-1" onClick={toUpdateProfile}>Edit Profile</Button>
                                     <Button outline className="ms-1" onClick={onLogout}>Log Out</Button>
+                                    <Button outline className="ms-1" onClick={modelOpen}>Reset Passwrod</Button>
                                 </div>
+                                <Modal show = {show} onHide = {handleClose}>
+                                  <Modal.Header closeButton>
+                                    <Modal.Title>Reset Password</Modal.Title>
+                                  </Modal.Header>
+                                  <Modal.Body>
+                                  <div
+                                    style={{
+                                      padding: "15px",
+                                      height: "fix-layout",
+                                      width: "fix-layout",
+                                    }}
+                                  >
+                                    <input
+                                      type="password"
+                                      class="form-control"
+                                      placeholder="Old Password"
+                                      onChange={onChangeOldPassword}
+                                    />
+                                    <input
+                                      type="password"
+                                      class="form-control"
+                                      placeholder="New Password"
+                                      style={{ marginTop: "10px" }}
+                                      onChange={onChangeNewPassword}
+                                    />
+                                  </div>
+                                  </Modal.Body>
+                                  <Modal.Footer>
+                                    <Button variant="secondary" onClick={handleClose}>
+                                      Close
+                                    </Button>
+                                    <Button
+                                      variant="primary"
+                                      disabled={!oldPassword || !newPassword}
+                                      onClick={resetPassword}
+                                    >
+                                      Save Changes
+                                    </Button>
+                                  </Modal.Footer>
+                                </Modal> 
                             </Card.Body>
                         </Card>
                         <Card className='mb-4 mb-lg-0'>
@@ -147,133 +183,8 @@ function Profile() {
                     </Col>
                 </Row>
             </Container>
-        </section>    
-      <div class="container rounded bg-white mt-5 mb-5">
-        <div class="row">
-          <div class="col-md-3 border-right">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-              <img
-                class="rounded-circle mt-5"
-                width="150px"
-                src={currentProfile.avatar}
-              />
-              <span> </span>
-            </div>
-          </div>
-          <div class="col-md-5 border-right">
-            <div class="p-3 py-5">
-              <div class="d-flex justify-content-between align-items-center mb-3">
-                <h4 class="text-right">Profile</h4>
-              </div>
-              <div class="row mt-2">
-                <div class="col-md-6">
-                  <label class="labels">First Name</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    value={currentProfile.firstName}
-                    disabled="true"
-                  />
-                </div>
-                <div class="col-md-6">
-                  <label class="labels">Last Name</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    value={currentProfile.lastName}
-                    disabled="true"
-                  />
-                </div>
-              </div>
-              <div class="row mt-3">
-                <div class="col-md-12">
-                  <label class="labels">Nick Name</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    value={userName}
-                    disabled="true"
-                  />
-                </div>
-                <div class="col-md-12">
-                  <label class="labels">Email</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    value={currentProfile.email}
-                    disabled="true"
-                  />
-                </div>
-              </div>
-              <div class="row mt-3">
-                <button
-                  class="btn btn-primary btn-lg btn-block"
-                  type="button"
-                  onClick={toUpdateProfile}
-                >
-                  Edit Profile
-                </button>
-                <button
-                  class=" btn btn-primary btn-lg btn-block mt-2"
-                  type="button"
-                  onClick={onLogout}
-                >
-                  Log Out
-                </button>
-                <button
-                  class=" btn btn-primary btn-lg btn-block mt-2"
-                  type="button"
-                  onClick={modelOpen}
-                >
-                  Reset Password
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Reset Password</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div
-            style={{
-              padding: "15px",
-              height: "fix-layout",
-              width: "fix-layout",
-            }}
-          >
-            <input
-              type="password"
-              class="form-control"
-              placeholder="Old Password"
-              onChange={onChangeOldPassword}
-            />
-            <input
-              type="password"
-              class="form-control"
-              placeholder="New Password"
-              style={{ marginTop: "10px" }}
-              onChange={onChangeNewPassword}
-            />
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button
-            variant="primary"
-            disabled={!oldPassword || !newPassword}
-            onClick={resetPassword}
-          >
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </>
-  );
+  )
 }
 
-export default Profile;
+export default Profile
