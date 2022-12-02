d030838010f0280d709ee4b6d88b1c6405de80b9
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-12-02 00:50:16 +0000

    Added About Sections to Homepage

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -1,102 +1,103 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { motion, useScroll } from "framer-motion";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
-import{
-  Container,
-  Row,
-  Col,
-  Card
-} from 'react-bootstrap'
-
-import './About.css'
-import GameEngineMedium from '../../Assets/Images/Game_Engine_Medium.PNG'
-import Game2D from '../../Assets/Images/game_art_1.PNG'
-
+import "./About.css";
+import GameEngineMedium from "../../Assets/Images/Game_Engine_Medium.PNG";
+import Game2D from "../../Assets/Images/game_art_1.PNG";
 
 function About() {
+  const { scrollYProgress } = useScroll();
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h2>Features</h2>
-          <p>
-            <em>Start your Game Dev Journey with the Horizon Game Engine</em>
-          </p>
-          <ul>
-            <li>
-              <strong>Custom API: </strong>
-              The Horizon The Horizon Engine uses it's own custom API making it platform and library agnostic.
-            </li>
-            <li>
-              <strong>Physics System: </strong>
-              Add rigid body physics to any objects added to the scene.
-            </li>
-            <li>
-              <strong>Visual Scripting: </strong>      
-              Add Scripts to game objects using our visual scripting!
-            </li>
-            <li>
-              <strong>Community Driven: </strong>
-              Join our community and learn more about the Horizon Game Engine using our community section
-            </li>
-          </ul>
-          <br></br>
-        </Col>
-        <Col>
-          <Card className='mt-5 mb-5'>
-            <Card.Img variant="bottom" src = {GameEngineMedium}/>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-      <Col>
-        <Card className='mb-5'>
-          <Card.Img variant="bottom" src = {Game2D}/>
-        </Card>
-      </Col>
-        <Col>
-          <h2>Create Amazing 2D Games</h2>
-          <p>
-            <em>Horizon is  purpose built for creating 2D Games</em>
-          </p>
-          <ul>
-            <li>
-            <strong>Multiple Camera Angles: </strong>
-            Developers will be able to utilise a top-down and side scroll camera angle in their games.
-            </li>
-            <li>
-            <strong>Audio: </strong>
-              Add audio to any game objects 
-            </li>
-            <li>
-            <strong>Logging & Debugging: </strong>
-              Check for any bugs or errors using our inbuilt logging system
-            </li>
-          </ul>
-          <br></br>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h2>Horizon Engine Scripting</h2>
-          <p>
-            <em>Customise your games with scripting</em>
-          </p>
-          <ul>
-            <li>
-              <strong>Visual Scripting: </strong>
-              Create logic for game obejcts using visual drag-and-drop graphs instead of writing code!
-            </li>
-            <li>
-              <strong>Focus on Creating Games: </strong>
-              Visual scripting empowers users to focus on creating awesome games not writing code 
-            </li>
-          </ul>
-        </Col>
-        <Col>
-        </Col>
-      </Row>
-    </Container>
-  )
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h2>Features</h2>
+            <p>
+              <em>Start your Game Dev Journey with the Horizon Game Engine</em>
+            </p>
+            <ul>
+              <li>
+                <strong>Custom API: </strong>
+                The Horizon The Horizon Engine uses it's own custom API making
+                it platform and library agnostic.
+              </li>
+              <li>
+                <strong>Physics System: </strong>
+                Add rigid body physics to any objects added to the scene.
+              </li>
+              <li>
+                <strong>Visual Scripting: </strong>
+                Add Scripts to game objects using our visual scripting!
+              </li>
+              <li>
+                <strong>Community Driven: </strong>
+                Join our community and learn more about the Horizon Game Engine
+                using our community section
+              </li>
+            </ul>
+            <br></br>
+          </Col>
+          <Col>
+            <Card className="mt-5 mb-5">
+              <Card.Img variant="bottom" src={GameEngineMedium} />
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card className="mb-5">
+              <Card.Img variant="bottom" src={Game2D} />
+            </Card>
+          </Col>
+          <Col>
+            <h2>Create Amazing 2D Games</h2>
+            <p>
+              <em>Horizon is purpose built for creating 2D Games</em>
+            </p>
+            <ul>
+              <li>
+                <strong>Multiple Camera Angles: </strong>
+                Developers will be able to utilise a top-down and side scroll
+                camera angle in their games.
+              </li>
+              <li>
+                <strong>Audio: </strong>
+                Add audio to any game objects
+              </li>
+              <li>
+                <strong>Logging & Debugging: </strong>
+                Check for any bugs or errors using our inbuilt logging system
+              </li>
+            </ul>
+            <br></br>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2>Horizon Engine Scripting</h2>
+            <p>
+              <em>Customise your games with scripting</em>
+            </p>
+            <ul>
+              <li>
+                <strong>Visual Scripting: </strong>
+                Create logic for game obejcts using visual drag-and-drop graphs
+                instead of writing code!
+              </li>
+              <li>
+                <strong>Focus on Creating Games: </strong>
+                Visual scripting empowers users to focus on creating awesome
+                games not writing code
+              </li>
+            </ul>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default About
+export default About;
--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, ListGroup } from "react-bootstrap";
 
 import "./Home.css";
 import PhysicsPic from "../../Assets/Images/physics_example.PNG";
 import ScenePic from "../../Assets/Images/scene_example.png";
 import ScriptingPic from "../../Assets/Images/scripting_example.jpg";
 import CommunityPic from "../../Assets/Images/Gamer_Dev_Pic.jpg";
+import GameEngineMedium from "../../Assets/Images/Game_Engine_Medium.PNG";
+import Game2D from "../../Assets/Images/game_art_1.PNG";
 
 // Row = 12 cols or 100%
 function Home() {
@@ -98,6 +100,90 @@ function Home() {
                 Learn More
               </Button>
             </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6} sm={6}>
+          <Card style = {{border: "none", padding: "50px"}}>
+            <Card.Title>Features</Card.Title>
+            <ListGroup>
+              <ListGroup.Item  style = {{border: "none", padding: "25px 25px" }}>
+                <strong>Custom API: </strong>
+                The Horizon The Horizon Engine uses it's own custom API making
+                it platform and library agnostic.
+              </ListGroup.Item>
+              <ListGroup.Item style = {{border: "none", padding: "25px 25px"  }}>
+                <strong>Physics System: </strong>
+                Add rigid body physics to any objects added to the scene.
+              </ListGroup.Item>
+              <ListGroup.Item style = {{border: "none", padding: "25px 25px" }}>
+                <strong>Scripting: </strong>
+                Add Scripts to game objects using scripting!
+              </ListGroup.Item>
+              <ListGroup.Item style = {{border: "none", padding: "25px 25px" }}>
+                <strong>Community Driven: </strong>
+                Join our community and learn more about the Horizon Game Engine
+                using our community section
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+        <Col lg={6} sm={6}>
+          <Card style = {{border: "none", paddingTop: "50px"}}>
+            <Card.Img variant="bottom" src={GameEngineMedium} />
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6} sm={6}>
+          <Card style = {{border: "none", paddingTop: "50px"}}>
+            <Card.Title>Creating 2D Games</Card.Title>
+            <ListGroup>
+              <ListGroup.Item style = {{ border: "none", padding: "25px 25px" }}>
+                <strong>Multiple Camera Angles: </strong>
+                Developers will be able to utilise a top-down and side scroll
+                camera angle in their games.
+              </ListGroup.Item>
+              <ListGroup.Item style = {{ border: "none", padding: "25px 25px" }}>
+                <strong>Audio: </strong>
+                Add audio to any game objects
+              </ListGroup.Item>
+              <ListGroup.Item style = {{ border: "none", padding: "25px 25px"  }}>
+                <strong>Logging & Debugging: </strong>
+                Check for any bugs or errors using our inbuilt logging system
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+        <Col lg={6} sm={6}>
+          <Card style = {{border: "none"}}>
+            <Card.Img variant="bottom" src={Game2D} />
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6} sm={6}>
+          <Card style = {{border: "none", paddingTop: "50px"}}>
+          <Card.Title>Scripting</Card.Title>
+            <ListGroup style = {{border: "none"}}>
+              <ListGroup.Item style = {{ border: "none",padding: "25px 25px" }}>
+                <strong>Visual Scripting: </strong>
+                Create logic for game obejcts using visual drag-and-drop graphs
+                instead of writing code!
+              </ListGroup.Item>
+              <ListGroup.Item style = {{ border: "none",padding: "25px 25px" }}>
+                <strong>Focus on Creating Games: </strong>
+                Visual scripting empowers users to focus on creating awesome
+                games not writing code
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+        <Col lg={6} sm={6}>
+          <Card style = {{border: "none"}}>
+            <Card.Title style = {{padding: "25px"}}>Picture</Card.Title>
+
           </Card>
         </Col>
       </Row>
