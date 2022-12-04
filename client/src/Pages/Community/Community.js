2fdab73704070dacfaae8f4b69e68e20b712b52c
prajakta-kulkarni-git <kulkarni.prajakta2612@gmail.com>
2022-12-04 18:44:46 +0000

    Implemented Answering to Answers

--- client/src/Pages/Community/Community.js
+++ client/src/Pages/Community/Community.js
@@ -27,9 +27,13 @@ function Community() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [tags, setTags] = useState([]);
+  const [isAll, setIsAll] = useState(true);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+
+  const [showLoading, setShowLoading] = useState(true);
 
   const authToken = localStorage.getItem("authToken");
   const { user } = useSelector((state) => state.auth);
@@ -38,13 +42,15 @@ function Community() {
 
   useEffect(() => {
     const init = async () => {
+      setShowLoading(true);
       const authToken = localStorage.getItem("authToken");
       [error, response] = await Api.getQuestions(
         authToken,
         page,
         sort.sort,
         sort.order,
-        search
+        search,
+        isAll
       );
       if (error) {
         console.log(error);
@@ -52,141 +58,160 @@ function Community() {
       if (response.status === 200) {
         setObj(response.data);
         console.log(response.data);
+        setShowLoading(false);
       }
       if (error) {
+        setShowLoading(false);
         console.log(error);
       }
     };
     init();
-  }, [sort, page, search, authToken, user, error, response]);
+  }, [sort, page, search, authToken, user, error, response, isAll]);
 
-    //onChange Handler
-    const onChangeTitle = (e) => {
-      setTitle(e.target.value);
-    };
-    const onChangeDescription = (e) => {
-      setDescription(e.target.value);
-    };
-    const onChangeTags = (e) => {
-      setTags(e.target.value);
-    };
-  
-    const askQuestionOnClickHandler = async () => {
-      const [err, res] = await Api.createQuestion(title, description, tags);
-      if (err) {
-        toast.error("Something went wrong.Please try again later!");
-      }
-      if (res) {
-        //   console.log(res);
-        toast.success("Question Created!");
-        navigate("/community");
-      }
-    };
+  //onChange Handler
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+  const onChangeTags = (e) => {
+    setTags(e.target.value);
+  };
 
-    function createPostandClose() {
-      handleClose()
-      askQuestionOnClickHandler()
-      window.location.reload()
+  const askQuestionOnClickHandler = async () => {
+    const [err, res] = await Api.createQuestion(title, description, tags);
+    if (err) {
+      toast.error("Something went wrong.Please try again later!");
     }
+    if (res) {
+      //   console.log(res);
+      toast.success("Question Created!");
+      navigate("/community");
+    }
+  };
+
+  function createPostandClose() {
+    handleClose()
+    askQuestionOnClickHandler()
+  }
   return (
-    <div className="community-container">
-      <Container fluid className="que-container">
-        <Row
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItem: "center",
-          }}
-        >
-          <div
+    <>
+      {
+        showLoading ? <div className="loadingDiv"></div> : ""
+      }
+      <div className="community-container">
+        <Container fluid className="que-container">
+
+          <Row
             style={{
               display: "flex",
-              width: "80%",
-              padding: "10px",
-              marginTop: "2%",
-              justifyContent: "space-around",
+              justifyContent: "center",
+              alignItem: "center",
             }}
           >
-            <div>
-              <Button variant="primary">All</Button>{" "}
-              <Button variant="primary">UnAnswered</Button>{" "}
-            </div>
+            <div
+              style={{
+                display: "flex",
+                width: "80%",
+                padding: "10px",
+                marginTop: "2%",
+                justifyContent: "space-around",
+              }}
+            >
+              <div>
+                {
+                  isAll == true ?
+                    <>
+                      <Button
+                        variant="primary" disabled={false} onClick={() => { setIsAll(!isAll) }}>All</Button>{" "}
+                      <Button variant="primary" disabled={true} onClick={() => { setIsAll(!isAll) }}>UnAnswered</Button>{" "}
+                    </> :
+                    <>
+                      <Button
+                        variant="primary" disabled={true} onClick={() => { setIsAll(!isAll) }}>All</Button>{" "}
+                      <Button variant="primary" disabled={false} onClick={() => { setIsAll(!isAll) }}>UnAnswered</Button>{" "}
+                    </>
+                }
 
-            <div style={{ display: "flex", marginTop: "-19px" }}>
-              <Search setSearch={(search) => setSearch(search)} />{" "}
-              <Sort sort={sort} setSort={(sort) => setSort(sort)} />
-            </div>
-            <div>
-              <Button
-                variant="primary"
-                onClick={handleShow}
-                disabled={!authToken}
-              >
-                Create a Post
-              </Button>
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Create Post</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <Form>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="exampleForm.ControlInput1"
-                    >
-                      <Form.Label>Title</Form.Label>
+              </div>
+
+              <div style={{ display: "flex", marginTop: "-19px" }}>
+                <Search setSearch={(search) => setSearch(search)} />{" "}
+                <Sort sort={sort} setSort={(sort) => setSort(sort)} />
+              </div>
+              <div>
+                <Button
+                  variant="primary"
+                  onClick={handleShow}
+                  disabled={!authToken}
+                >
+                  Create a Post
+                </Button>
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Create Post</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <Form>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlInput1"
+                      >
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                          type="title"
+                          placeholder="Title"
+                          autoFocus
+                          onChange={onChangeTitle}
+                        />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1"
+                      >
+                        <Form.Label>Post Body</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows={3}
+                          onChange={onChangeDescription}
+                        />
+                      </Form.Group>
+                    </Form>
+                    <Form.Group>
+                      <Form.Label>Tags</Form.Label>
                       <Form.Control
-                        type="title"
-                        placeholder="Title"
-                        autoFocus
-                        onChange={onChangeTitle}
+                        placeholder="Separated by Comma"
+                        onChange={onChangeTags}
                       />
                     </Form.Group>
-                    <Form.Group
-                      className="mb-3"
-                      controlId="exampleForm.ControlTextarea1"
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Close
+                    </Button>
+                    <Button
+                      variant="primary"
+                      onClick={createPostandClose}
                     >
-                      <Form.Label>Post Body</Form.Label>
-                      <Form.Control 
-                        as="textarea" 
-                        rows={3}
-                        onChange = {onChangeDescription} 
-                      />
-                    </Form.Group>
-                  </Form>
-                  <Form.Group>
-                    <Form.Label>Tags</Form.Label>
-                    <Form.Control 
-                      placeholder="Separated by Comma"
-                      onChange={onChangeTags} 
-                    />
-                  </Form.Group>
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                  <Button 
-                    variant="primary" 
-                    onClick={createPostandClose}
-                  >
-                    Create Post
-                  </Button>
-                </Modal.Footer>
-              </Modal>
+                      Create Post
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </div>
             </div>
-          </div>
-        </Row>
+          </Row>
 
-        <Question questions={obj.questions ? obj.questions : []} />
-        <Pagination
-          page={page}
-          limit={obj.limit ? obj.limit : 0}
-          total={obj.total ? obj.total : 0}
-          setPage={(page) => setPage(page)}
-        />
-      </Container>
-    </div>
+          <Question questions={obj.questions ? obj.questions : []} />
+          <Pagination
+            page={page}
+            limit={obj.limit ? obj.limit : 0}
+            total={obj.total ? obj.total : 0}
+            setPage={(page) => setPage(page)}
+          />
+        </Container>
+      </div>
+    </>
   );
 }
 export default Community;
