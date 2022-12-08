f577bd8f6aa6f607191b6d6f2be14ae8f168ec6d
Ming <gaominghui464@outlook.com>
2022-12-08 22:22:34 +0000

    add validation for tags of posts

--- client/src/validation/postValidation.js
+++ client/src/validation/postValidation.js
@@ -3,7 +3,10 @@ import * as yup from 'yup';
 export const createPostSchema = yup.object().shape({
     title: yup.string().required("Please enter a title"),
     description: yup.string().required("Please enter description"),
-    tags: yup.string().required("Please enter tag"),
+    tags: yup.string().required("Please enter tag").matches(
+        /^([\,a-zA-Z0-9]{1,30})$/,
+        "tags must not be longer than 30 characters, only numbers and letters, separated by comma"
+    ),
 });
 
 export const createAnswerSchema = yup.object().shape({
